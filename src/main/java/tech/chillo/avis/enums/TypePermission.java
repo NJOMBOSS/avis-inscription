edd7f1338533ae885e044c03f7503dbcae61cd29
NJOMBOSS <fonke1982@gmail.com>
2023-12-01 12:04:13 +0100

    roles et permissions

---
+++ src/main/java/tech/chillo/avis/enums/TypePermission.java
@@ -0,0 +1,24 @@
+package tech.chillo.avis;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum TypePermission {
+
+    ADMINISTRATEUR_CREATE,
+    ADMINISTRATEUR_READ,
+    ADMINISTRATEUR_UPDATE,
+    ADMINISTRATEUR_DELETE,
+
+    MANAGER_CREATE,
+    MANAGER_READ,
+    MANAGER_UPDATE,
+    MANAGER_DELETE_AVIS,
+
+    UTILISATEUR_CREATE_AVIS;
+
+
+    private String permission;
+}
